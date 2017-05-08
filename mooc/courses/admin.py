97ddc1cfeff22bab4511a1d46f33b377472e1bd0
smk_barbosa <samuka1@gmail.com>
2017-05-08 18:44:28 -0300

    Customizando exibição do curso no painel de administração

--- mooc/courses/admin.py
+++ mooc/courses/admin.py
@@ -5,7 +5,7 @@ from .models import Course
 
 class CourseAdmin(admin.ModelAdmin):
 
-    list_display = ['name', 'slug', 'start_course', 'created_at']
+    list_display = ['name', 'slug', 'start_date', 'create_at']
     search_fields = ['name', 'slug']
 
 admin.site.register(Course, CourseAdmin)
