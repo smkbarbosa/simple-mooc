cabe8199cc2e65dd72a973b19c46864e9764329b
smk_barbosa <samuka1@gmail.com>
2017-05-09 12:04:59 -0300

    implementa listagem de cursos e contexto

--- mooc/courses/views.py
+++ mooc/courses/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from .models import Course
+
 # Create your views here.
 
 def index(request):
+    courses = Course.objects.all()
     template_name = 'courses/index.html'
-    return render(request, template_name)
+    context = {
+        'courses': courses
+    }
+    return render(request, template_name, context)
