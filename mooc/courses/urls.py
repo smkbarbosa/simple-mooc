3327c859de12b0febcc67a4693ea7c66a4023085
smk_barbosa <samuka1@gmail.com>
2017-05-10 11:16:26 -0300

    alterando url de id para slug

--- mooc/courses/urls.py
+++ mooc/courses/urls.py
@@ -5,6 +5,7 @@ from mooc.courses import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<pk>\d+)/$', views.details, name='details')
+    url(r'^(?P<slug>[\w_-]+)/$', views.details, name='details')
+    #url(r'^(?P<pk>\d+)/$', views.details, name='details')
     #url(r'^contato/', views.contact, name='contato'),
 ]
