1c9027befbef498eb935dd87d31ab0b81b1ab435
smk_barbosa <samuka1@gmail.com>
2017-05-10 09:28:02 -0300

    implementa caminhos para imagens enviadas pelo usuario na criação do curso

--- mooc/urls.py
+++ mooc/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     url(r'^', include('mooc.core.urls', namespace='core')),
@@ -22,6 +26,9 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 """
     url(r'^$', views.home, name='home'),
