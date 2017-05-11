da17f5dfc340cb48fc9e100fd6730d07714f6a4f
smk_barbosa <samuka1@gmail.com>
2017-05-11 16:27:33 -0300

    Ajustes no form de contato

--- mooc/courses/views.py
+++ mooc/courses/views.py
@@ -24,13 +24,15 @@ def index(request):
 
 def details(request, slug):
     course = get_object_or_404(Course, slug=slug)
+    context = {}
     if request.method == 'POST':
         form = ContactCourse(request.POST)
+        if form.is_valid():
+            context['is_valid'] = True
+            form = ContactCourse()
     else:
         form = ContactCourse()
-    context = {
-        'course': course,
-        'form': form
-    }
+    context['form'] = form
+    context['course'] = course
     template_name = 'courses/details.html'
     return render(request, template_name, context)
