4da7a069e09c7064f411ab67214c6bbf151b8d93
smk_barbosa <samuka1@gmail.com>
2017-05-11 16:11:17 -0300

    Implementa formulário de contato

--- mooc/courses/views.py
+++ mooc/courses/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Course
 
+from .forms import ContactCourse
+
 # Create your views here.
 
 def index(request):
@@ -22,8 +24,13 @@ def index(request):
 
 def details(request, slug):
     course = get_object_or_404(Course, slug=slug)
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)
+    else:
+        form = ContactCourse()
     context = {
-        'course': course
+        'course': course,
+        'form': form
     }
     template_name = 'courses/details.html'
     return render(request, template_name, context)
